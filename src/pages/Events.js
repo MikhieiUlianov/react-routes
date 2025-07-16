a1d325c34b086877d62d810fafb7b03d4096480a
Mkh <example@gmail.com>
2025-07-16 18:21:27 +0200

    json-utility-function

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,17 +1,13 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  //it checks if it get a promise, and will automaticly get the result of promise
   const data = useLoaderData();
   const events = data.events;
   //loader automaticly parse data from response
 
-  /*   if(events.error) {
-    return <p>{events.message}</p>
-  } */
-  return <>{<EventsList events={events} />}</>;
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
@@ -20,15 +16,17 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    /* 
-    return { isError: true, message: "Could not fetch events." }; */
-
     //when an error gets thrown in a loader, react behaves differently.
     //react router will simply render the closest error element, which we adding as "errorElement" prop
     //"errorElement" is not only for unexisting routes, it also includes loaders error
-    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
-      status: 500,
-    });
+
+    //use throw Response, when we want to manage error by ourselfs,  send certain message/status.
+    //throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    // status: 500,
+    //});
+
+    //this syntaxc is not availible in router v7, then we must use longer version instead
+    return json({ message: "Could not fetch events." }, { status: 500 });
   } else {
     return response;
   }
