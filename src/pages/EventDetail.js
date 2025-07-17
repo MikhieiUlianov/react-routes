2f7534e16c17bb34af099ac2097d1e190e626b73
Mkh <example@gmail.com>
2025-07-17 16:55:01 +0200

    controllig-loading-several-requests

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -1,28 +1,83 @@
-import { redirect, useRouteLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { useRouteLoaderData, redirect, Await } from "react-router-dom";
 
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
 
 function EventDetailPage() {
-  const data = useRouteLoaderData("event-detail");
+  const { event, events } = useRouteLoaderData("event-detail");
 
-  return <EventItem event={data.event} />;
+  return (
+    <>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
+    </>
+  );
 }
 
 export default EventDetailPage;
 
-export async function loader({ request, params }) {
-  const id = params.eventId;
-
+async function loadEvent(id) {
   const response = await fetch("http://localhost:8080/events/" + id);
 
   if (!response.ok) {
+    throw new Response(
+      JSON.stringify({
+        message: "Could not fetch details for selected event.",
+      }),
+      {
+        status: 500,
+      }
+    );
+  } else {
+    const resData = await response.json();
+    return resData.event;
+  }
+}
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return { isError: true, message: 'Could not fetch events.' };
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+    //   status: 500,
+    // });
     throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
       status: 500,
     });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
 }
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+
+  return {
+    //await makes sure that it will wait until this component will be rendered,
+    //  and only after this it will show other static parts of page,
+    //  while other requests will be loading
+
+    //before moving, before loading this page component at all, navigating to this page component,
+    event: await loadEvent(id),
+    //but will load this data, after the page was loaded
+    events: loadEvents(),
+
+    //so with "await" we define which data should be loaded before moving to this page,
+    //  and which data must be render after the page was loaded
+  };
+}
+
 export async function action({ params, request }) {
   const eventId = params.eventId;
   const response = await fetch("http://localhost:8080/events/" + eventId, {
