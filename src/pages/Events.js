8fba21f03822ef53825a66f66a3581b305eabe4a
Mkh <example@gmail.com>
2025-07-17 16:18:27 +0200

    defered-request

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,15 +1,21 @@
-import { useLoaderData } from "react-router-dom";
+import { Await, useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 export default function EventsPage() {
-  const data = useLoaderData();
-  const events = data.events;
+  const { events } = useLoaderData();
 
-  return <EventsList events={events} />;
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadEvents) => <EventsList events={loadEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -17,6 +23,13 @@ export async function loader() {
       status: 500,
     });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export function loader() {
+  return {
+    events: loadEvents(),
+  };
 }
